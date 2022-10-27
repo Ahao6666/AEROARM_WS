8ffbbaa0c7ded15c914a6c9d0cd0e0f4a012d633
Ahao6666 <ahousemail@163.com>
2022-10-27 19:23:57 +0800

    change the order of the gripper

--- src/load_capture/load_capture.cpp
+++ src/load_capture/load_capture.cpp
@@ -23,25 +23,16 @@ int main(int argc, char **argv)
 
     std_msgs::Float32 delta1_pos, delta2_pos, delta3_pos;
     std_msgs::Float32 gripper1_pos,gripper2_pos;
-    ros::Rate loop_rate(0.5);
+    ros::Rate loop_rate(1);
 
     while (ros::ok())
     {
-
-        gripper1_pos.data = 2;
-        gripper2_pos.data = 2;
+        gripper1_pos.data = 1;
+        gripper2_pos.data = 1;
         gripper1_pub.publish(gripper1_pos);
         gripper2_pub.publish(gripper2_pos);
         loop_rate.sleep();
-        
-        delta1_pos.data = 0.5;
-        delta2_pos.data = 0.5;
-        delta3_pos.data = 0.5;
-        delta1_pub.publish(delta1_pos);
-        delta2_pub.publish(delta2_pos);
-        delta3_pub.publish(delta3_pos);
-        loop_rate.sleep();
-        
+
         gripper1_pos.data = -0.3;
         gripper2_pos.data = -0.3;
         gripper1_pub.publish(gripper1_pos);
@@ -54,8 +45,17 @@ int main(int argc, char **argv)
         delta1_pub.publish(delta1_pos);
         delta2_pub.publish(delta2_pos);
         delta3_pub.publish(delta3_pos);
-        // 按照循环频率延时
         loop_rate.sleep();
+
+        delta1_pos.data = 0.5;
+        delta2_pos.data = 0.5;
+        delta3_pos.data = 0.5;
+        delta1_pub.publish(delta1_pos);
+        delta2_pub.publish(delta2_pos);
+        delta3_pub.publish(delta3_pos);
+        loop_rate.sleep();
+
+        // 按照循环频率延时
         ROS_INFO("Publsh load capture test ");
 
     }
