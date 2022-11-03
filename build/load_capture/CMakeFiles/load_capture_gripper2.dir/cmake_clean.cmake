9b4d4b92efddab2a0de35f945612a207a9151687
Ahao6666 <ahousemail@163.com>
2022-11-03 19:07:37 +0800

    add load_capture_gripper2

---
+++ build/load_capture/CMakeFiles/load_capture_gripper2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/load_capture_gripper2.dir/load_capture_gripper2.cpp.o"
+  "/home/ahao/catkin_ws/devel/.private/load_capture/lib/load_capture/load_capture_gripper2.pdb"
+  "/home/ahao/catkin_ws/devel/.private/load_capture/lib/load_capture/load_capture_gripper2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/load_capture_gripper2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
